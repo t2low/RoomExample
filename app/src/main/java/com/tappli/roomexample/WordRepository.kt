e34785f555ed017f2226f1354e2cfa3e779cb37c
Tetsuro Nakamura <t2low.nk@gmail.com>
2020-06-03 01:39:19 +0900

    リポジトリの作成

---
+++ app/src/main/java/com/tappli/roomexample/WordRepository.kt
@@ -0,0 +1,12 @@
+package com.tappli.roomexample
+
+import androidx.lifecycle.LiveData
+
+class WordRepository(private val wordDao: WordDao) {
+
+    val allWords: LiveData<List<Word>> = wordDao.getAlphabetizedWords()
+
+    suspend fun insert(word: Word) {
+        wordDao.insert(word)
+    }
+}
