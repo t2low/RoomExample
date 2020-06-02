b00e41062ac6e3dd375ceda576c0bab2b0c42e86
Tetsuro Nakamura <t2low.nk@gmail.com>
2020-06-03 02:17:03 +0900

    RecyclerViewの追加
    https://codelabs.developers.google.com/codelabs/android-room-with-a-view-kotlin/#11

--- app/src/main/java/com/tappli/roomexample/MainActivity.kt
+++ app/src/main/java/com/tappli/roomexample/MainActivity.kt
@@ -2,11 +2,17 @@ package com.tappli.roomexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = WordListAdapter(this)
+        recyclerView.adapter= adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
