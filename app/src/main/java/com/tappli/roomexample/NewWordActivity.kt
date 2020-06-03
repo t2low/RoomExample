7fb7728bf3fb8536b4f0c6a9506ce95653434b9a
Tetsuro Nakamura <t2low.nk@gmail.com>
2020-06-04 01:40:58 +0900

    言葉を追加するActivityを追加

--- app/src/main/java/com/tappli/roomexample/NewWordActivity.kt
+++ app/src/main/java/com/tappli/roomexample/NewWordActivity.kt
@@ -1,12 +1,36 @@
 package com.tappli.roomexample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class NewWordActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_REPLY = "com.tappli.roomexample.REPLY"
+    }
+
+    private lateinit var editWordView: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_word)
+        editWordView = findViewById(R.id.edit_word)
+
+        val button = findViewById<Button>(R.id.button_save)
+        button.setOnClickListener {
+            val replyIntent = Intent()
+            if (editWordView.text.isEmpty()) {
+                setResult(Activity.RESULT_CANCELED, replyIntent)
+            } else {
+                val word = editWordView.text.toString()
+                replyIntent.putExtra(EXTRA_REPLY, word)
+                setResult(RESULT_OK, replyIntent)
+            }
+            finish()
+        }
     }
 }
