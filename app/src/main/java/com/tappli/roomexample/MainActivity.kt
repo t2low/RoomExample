698b011c3820f7aed45947e9ad6c7bcc3c45da42
Tetsuro Nakamura <t2low.nk@gmail.com>
2020-06-04 01:56:03 +0900

    Activity間、およびViewModelをつなぐ

--- app/src/main/java/com/tappli/roomexample/MainActivity.kt
+++ app/src/main/java/com/tappli/roomexample/MainActivity.kt
@@ -1,18 +1,48 @@
 package com.tappli.roomexample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var wordViewModel: WordViewModel
+    private val newWordActivityRequestCode = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val adapter = WordListAdapter(this)
-        recyclerView.adapter= adapter
+        recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        wordViewModel = ViewModelProvider(this).get(WordViewModel::class.java)
+        wordViewModel.allWords.observe(this, Observer { words ->
+            words?.let { adapter.setWords(it) }
+        })
+
+        fab.setOnClickListener {
+            val intent = Intent(this, NewWordActivity::class.java)
+            startActivityForResult(intent, newWordActivityRequestCode)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == newWordActivityRequestCode && resultCode == RESULT_OK) {
+            data?.getStringExtra(NewWordActivity.EXTRA_REPLY)?.let {
+                val word = Word(it)
+                wordViewModel.insert(word)
+            }
+        } else {
+            Toast.makeText(applicationContext, R.string.empty_not_saved, Toast.LENGTH_SHORT).show()
+        }
     }
 }
